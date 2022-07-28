66b72a8a77e56eaff2ed8c36fdad5a74e12a2399
Davi Fiquer <davifiquer@gmail.com>
2022-07-28 14:22:46 -0300

    Arquivos adicionados manualmente
    create_img_news.py = Arquivo principal, onde o código é executado
    noticias.py = Código que realiza o webscraping das notícias
    template_news.png = É o template padrão da imagem

---
+++ noticias.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+# bibliotecas importadas
+from datetime import datetime
+# from time import sleep
+# import datetime as dt
+import time
+import arrow
+import requests
+from bs4 import BeautifulSoup
+
+def calendario(url):
+    # processo de requisiçao de dados no site -->
+    url = url  # site utilizado no webscraping
+
+    cabecalho = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 '
+                      'Safari/537.36'}
+
+    requisicao = requests.get(url, headers=cabecalho)  # requisiçao dentro do site
+
+    # processo de requisiçao de dados no site <--
+
+    # processo de tratamento do html -->
+
+    soup = BeautifulSoup(requisicao.text, 'html.parser')  # tratamento do html com o módulo 'bs4'
+
+    tabela = soup.find('table', {'id': 'economicCalendarData'})  # apenas a tabela com o id especifico
+
+    corpo = tabela.find('tbody')  # apenas o corpo da tabela
+
+    linhas = corpo.findAll('tr', {'class': 'js-event-item'})  # apenas as linhas da tabela
+
+    calendario = []  # lista para as noticias
+
+    # 'tr' linha dentro da tabela html
+    # 'td' coluna dentro da tabela html
+
+    for tr in linhas:
+        horario = tr.attrs['data-event-datetime']  # separando o horario da noticia pela tag html 'data-event-datetime'
+        horario = arrow.get(horario,
+                            'YYYY/MM/DD HH:mm:ss')  # converter uma string de horario em um formato aceito pelo python
+        horario = horario.strftime('%H:%M')
+        calendario.append(horario)
+
+        horario = tr.attrs['data-event-datetime']  # separando o horario da noticia pela tag html 'data-event-datetime'
+        horario = arrow.get(horario,
+                            'YYYY/MM/DD HH:mm:ss')  # converter uma string de horario em um formato aceito pelo python
+        horas = (int(horario.strftime('%H')) * 60)
+        minutos = int(horario.strftime('%M'))
+        verificacao = horas + minutos  # horario em minutos para verrificar o tempo em minutos para envio da noticia
+        calendario.append(verificacao)
+
+        coluna = tr.find('td', {'class': 'flagCur'})  # separando o pais da noticia pela tag html 'flagCur'
+        bandeira = coluna.find('span')
+        calendario.append(bandeira.get('title'))
+
+        impacto = tr.find('td', {'class': 'sentiment'})
+        touro = impacto.findAll('i', {
+            'class': 'grayFullBullishIcon'})  # separando o impacto da noticia pela tag html 'grayFullBullishIcon' e sua quantidade respectiva
+        calendario.append(len(touro))
+
+        evento = tr.find('td', {'class': 'event'})
+        a = evento.find('a')  # separando a tag html especifica 'a' para obter o nome e a url da noticia
+
+        calendario.append('{}{}'.format(url, a[
+            'href']))  # separando a url da noticia com o url do site e tag de referencia html 'href'
+
+        calendario.append(a.text.strip())  # separando a chamada na notica pela tag html 'a' (texto dentro da tag)
+
+        # processo de tratamento do html <--
+
+    return calendario  # retorna a lista com as noticias
+
+
+# uso dos dados em html tratados em variaveis -->
+
+if __name__ == '__main__':
+    dados = (calendario('https://br.investing.com/economic-calendar/'))  # dados obtidos do html
+    
+    print(dados)  # lista com os dados
+    print('')
+    
+    quantidade = (len(dados) / 6)  # quantidade de noticias
+    
+    while True:
+    
+        horario = dados[0]  # dado especifico para o horario da
+        verificacao = dados[1]
+        pais = dados[2]  # dado especifico para o pais da noticia
+        impacto = dados[3]  # dado especifico para o impacto da noticia
+        link = dados[4]  # dado especifico para o link da noticia
+        chamada = dados[5]  # dado especifico para a chamada da noticia
+    
+        if impacto == 2 or impacto == 3:
+            # objeto_datetime = datetime.strptime(horario, '%H:%M')
+            # objeto_datetime.strftime('%H:%M')
+            # hora_agora = datetime.now()
+            # if objeto_datetime.time() > hora_agora.time():
+            #     print('A hora é maior!')
+            # else:
+            #     print('A hora é menor!')
+            noticia = (f'Local: {pais}\
+            \nHorário: {horario}\
+            \nImpacto da notícia: {impacto}\
+            \nNotícia: {chamada}\
+            \nPara ver mais acesse: {link}\
+            \n').strip()  # noticia formatada
+            print(noticia)
+            print('')
+    
+        for item in range(0, 6):
+            del dados[0]  # apaga as ultimas informaçoes ja usadas(6 primeiros itens na lista), para nao ter repetiçoes
+    
+        quantidade = quantidade - 1
+    
+        if quantidade == 0:
+            break
+        else:
+            pass
